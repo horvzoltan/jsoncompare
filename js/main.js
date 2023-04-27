e46ec451cd60bec57a8ac20afe383c089645cac2
Zoltan Horvath <Zoltan.Horvath2@dmgmori-heitec.hu>
2023-04-27 16:02:11 +0200

    bugfix

--- js/main.js
+++ js/main.js
@@ -4,54 +4,52 @@ const longerKeyContainer = document.getElementById('result3');
 
 let firstAsObject = null;
 let secondAsObject = null;
-
-const longerValues = [];
+let longerValues = [];
 
 document.getElementById("selectFile").addEventListener("change", function (ev) {
-  let file = ev.currentTarget.files[0];
-
-  // Abort file reading if no file was selected
-  if (!file) return;
-
-  readFileAsText(file).then((fileContent) => {
-    // Print file content on the console
-    firstFileContainer.value = fileContent;
-  });
+  loadFile(ev, firstFileContainer);
 }, false);
 
 document.getElementById("selectSecondFile").addEventListener("change", function (ev) {
-  let file = ev.currentTarget.files[0];
-
-  // Abort file reading if no file was selected
-  if (!file) return;
-
-  readFileAsText(file).then((fileContent) => {
-    secondFileContainer.value = fileContent;
-  });
+  loadFile(ev, secondFileContainer);
 }, false);
 
+document.getElementById('reset').addEventListener("click", function () {
+  reset();
+})
 document.getElementById("validate").addEventListener("click", function (ev) {
   if (!firstFileContainer.value || !secondFileContainer.value) {
     document.getElementById("error").textContent = 'You need to select 2 files.'
     return
   }
-  ;
   document.getElementById("error").textContent = '';
   firstAsObject = JSON.parse(firstFileContainer.value);
   secondAsObject = JSON.parse(secondFileContainer.value);
   const validElement = document.getElementById("valid");
-
+  console.log('elotte');
   if (shallowEqual(firstAsObject, secondAsObject)) {
     validElement.textContent = 'The json file structure is identical!'
   } else {
     validElement.style.color = 'red';
     validElement.textContent = 'The json file structure is not identical!'
   }
-
+  console.log('utana');
   iterate(firstAsObject, secondAsObject);
+  console.log('3adjara');
+  console.log(longerValues);
+  console.log(longerValues.join(' || '));
   longerKeyContainer.textContent = longerValues.join(' || ');
 }, false);
 
+const reset = () => {
+  document.getElementById('small-form').reset();
+  document.getElementById('valid').textContent = '';
+  document.getElementById('error').textContent = '';
+  document.getElementById('result3').textContent = '';
+  firstAsObject = null;
+  secondAsObject = null;
+  longerValues = [];
+}
 
 const iterate = (obj, obj2) => {
   Object.keys(obj).forEach(key => {
@@ -59,7 +57,7 @@ const iterate = (obj, obj2) => {
       iterate(obj[key], obj2[key]);
     }
 
-    if(obj[key].length < obj2[key].length){
+    if (obj[key].length < obj2[key].length) {
       longerValues.push(key);
     }
   })
@@ -82,6 +80,19 @@ function shallowEqual(object1, object2) {
   return true;
 }
 
+const loadFile = (ev, container) => {
+  let file = ev.currentTarget.files[0];
+  document.getElementById('result3').textContent = '';
+  longerValues = [];
+  // Abort file reading if no file was selected
+  if (!file) return;
+
+  readFileAsText(file).then((fileContent) => {
+    // Print file content on the console
+    container.value = fileContent;
+  });
+}
+
 function readFileAsText(file) {
   return new Promise(function (resolve, reject) {
     let fr = new FileReader();
@@ -97,3 +108,4 @@ function readFileAsText(file) {
     fr.readAsText(file);
   });
 }
+
