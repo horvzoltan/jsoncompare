095bdf5b4b9defcc329ab4e2cfdb050f0711ac05
horvzoltan <Zoltan.Horvath2@dmgmori-heitec.hu>
2023-04-28 13:19:45 +0200

    bugfix, download feature.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ let firstAsObject = null;
 let secondAsObject = null;
 let longerValues = [];
 
+
 document.getElementById("selectFile").addEventListener("change", function (ev) {
   loadFile(ev, firstFileContainer);
 }, false);
@@ -16,6 +17,11 @@ document.getElementById("selectSecondFile").addEventListener("change", function 
 
 document.getElementById('reset').addEventListener("click", function () {
   reset();
+});
+
+document.getElementById('download').addEventListener("click", function () {
+  if(longerValues.length === 0) return;
+  downloadResult();
 })
 document.getElementById("validate").addEventListener("click", function (ev) {
   if (!firstFileContainer.value || !secondFileContainer.value) {
@@ -26,20 +32,20 @@ document.getElementById("validate").addEventListener("click", function (ev) {
   firstAsObject = JSON.parse(firstFileContainer.value);
   secondAsObject = JSON.parse(secondFileContainer.value);
   const validElement = document.getElementById("valid");
-  console.log('elotte');
+
   if (shallowEqual(firstAsObject, secondAsObject)) {
     validElement.textContent = 'The json file structure is identical!'
   } else {
     validElement.style.color = 'red';
     validElement.textContent = 'The json file structure is not identical!'
   }
-  console.log('utana');
   iterate(firstAsObject, secondAsObject);
-  console.log('3adjara');
-  console.log(longerValues);
-  console.log(longerValues.join(' || '));
-  longerKeyContainer.textContent = longerValues.join(' || ');
+  longerKeyContainer.textContent = JSON.stringify(longerValues, null, "\t");
 }, false);
+
+const downloadResult = () => {
+  download('result',JSON.stringify(longerValues, null, "\t"));
+}
 
 const reset = () => {
   document.getElementById('small-form').reset();
@@ -57,8 +63,8 @@ const iterate = (obj, obj2) => {
       iterate(obj[key], obj2[key]);
     }
 
-    if (obj[key].length < obj2[key].length) {
-      longerValues.push(key);
+    if (obj[key]?.length < obj2[key]?.length) {
+      longerValues.push({key: key , value: obj2[key]});
     }
   })
 }
@@ -107,5 +113,18 @@ const readFileAsText = (file) => {
 
     fr.readAsText(file);
   });
+}
+
+const download = (filename,text) => {
+  let element = document.createElement('a');
+  element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
 }
 
