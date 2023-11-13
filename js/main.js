2938aa1e6aad79e8d55c26761e66583ae960a9ff
horvzoltan <Zoltan.Horvath2@dmgmori-heitec.hu>
2023-11-13 16:18:42 +0100

    fix runtime

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-const firstFileContainer = document.getElementById('result1');
-const secondFileContainer = document.getElementById('result2');
-const longerKeyContainer = document.getElementById('result3');
+import "regenerator-runtime/runtime";
+const firstFileContainer = document.getElementById("result1");
+const secondFileContainer = document.getElementById("result2");
+const longerKeyContainer = document.getElementById("result3");
 const validElement = document.getElementById("valid");
 const errorElement = document.getElementById("error");
 
@@ -10,39 +11,41 @@ let secondFile = null;
 let isValid;
 
 document.getElementById("selectFile").addEventListener("change", function (ev) {
-  loadFile(ev, firstFileContainer)
-    .then(array => {
-      files = array;
-      files.forEach((f)=>{
-        combineTwoFiles(files[0],f);
-      })
-    })
-});
-
-document.getElementById("selectSecondFile").addEventListener("change", function (ev) {
-  loadFile(ev, secondFileContainer).then((array) => {
-    secondFile = array[0];
-    validateStructure(files[0], secondFile).then(p => {
-      isValid = p;
+  loadFile(ev, firstFileContainer).then((array) => {
+    files = array;
+    files.forEach((f) => {
+      combineTwoFiles(files[0], f);
     });
   });
 });
 
-document.getElementById('reset').addEventListener("click", function () {
+document
+  .getElementById("selectSecondFile")
+  .addEventListener("change", function (ev) {
+    loadFile(ev, secondFileContainer).then((array) => {
+      secondFile = array[0];
+      validateStructure(files[0], secondFile).then((p) => {
+        isValid = p;
+      });
+    });
+  });
+
+document.getElementById("reset").addEventListener("click", function () {
   reset();
 });
 
-document.getElementById('download').addEventListener("click", function () {
+document.getElementById("download").addEventListener("click", function () {
   if (result.length === 0) return;
   downloadResult();
 });
 
 document.getElementById("validate").addEventListener("click", function (ev) {
   if (!files || !secondFile || !isValid) {
-    errorElement.textContent = 'You need to select files in both files selectors.'
-    return
+    errorElement.textContent =
+      "You need to select files in both files selectors.";
+    return;
   }
-  errorElement.textContent = '';
+  errorElement.textContent = "";
   firstFileContainer.value = JSON.stringify(files[0], null, "\t");
   secondFileContainer.value = JSON.stringify(secondFile, null, "\t");
   iterate(files[0], secondFile);
@@ -50,50 +53,50 @@ document.getElementById("validate").addEventListener("click", function (ev) {
 });
 
 const downloadResult = () => {
-  download('result', JSON.stringify(result, null, "\t"));
-}
+  download("result", JSON.stringify(result, null, "\t"));
+};
 
 const validateStructure = async (obj, obj2) => {
   if (shallowEqual(obj, obj2)) {
-    validElement.textContent = 'The json file structure is identical!'
+    validElement.textContent = "The json file structure is identical!";
     return true;
   }
-  validElement.style.color = 'red';
-  validElement.textContent = 'The json file structure is not identical!'
+  validElement.style.color = "red";
+  validElement.textContent = "The json file structure is not identical!";
   return false;
-}
+};
 
 const reset = () => {
-  document.getElementById('small-form').reset();
-  document.getElementById('valid').textContent = '';
-  document.getElementById('error').textContent = '';
-  longerKeyContainer.textContent = '';
+  document.getElementById("small-form").reset();
+  document.getElementById("valid").textContent = "";
+  document.getElementById("error").textContent = "";
+  longerKeyContainer.textContent = "";
   files = null;
   result = [];
-}
+};
 
 const iterate = (obj, obj2) => {
-  Object.keys(obj).forEach(key => {
-    if (typeof obj[key] === 'object' && obj[key] !== null) {
+  Object.keys(obj).forEach((key) => {
+    if (typeof obj[key] === "object" && obj[key] !== null) {
       iterate(obj[key], obj2[key]);
     }
     if (obj[key]?.length < obj2[key]?.length) {
-      result.push({key: key, value: obj2[key]});
+      result.push({ key: key, value: obj2[key] });
     }
-  })
-}
+  });
+};
 
 const combineTwoFiles = (obj, obj2) => {
-  Object.keys(obj).forEach(key => {
-    if (typeof obj[key] === 'object' && obj[key] !== null) {
+  Object.keys(obj).forEach((key) => {
+    if (typeof obj[key] === "object" && obj[key] !== null) {
       combineTwoFiles(obj[key], obj2[key]);
     }
 
     if (obj[key]?.length < obj2[key]?.length) {
       obj[key] = obj2[key];
     }
-  })
-}
+  });
+};
 
 const shallowEqual = (object1, object2) => {
   const keys1 = Object.keys(object1);
@@ -110,37 +113,43 @@ const shallowEqual = (object1, object2) => {
   }
 
   return true;
-}
+};
 
 // Function for file loading
 const loadFile = async (ev, container) => {
   let currentFiles = [];
   let filesTestArray = [];
 
-  await Array.from(ev.currentTarget.files).forEach(file => currentFiles.push(file));
+  await Array.from(ev.currentTarget.files).forEach((file) =>
+    currentFiles.push(file)
+  );
 
-  longerKeyContainer.textContent = '';
+  longerKeyContainer.textContent = "";
   result = [];
-
 
   // Abort file reading if no file was selected
   if (!currentFiles[0]) return;
 
   for (const value of currentFiles) {
     const index = currentFiles.indexOf(value);
-      if (index > 0) {
-        if (!await validateStructure(filesTestArray[0], await readFileAsText(value))) {
-          console.log('123')
-          return;
-        }
+    if (index > 0) {
+      if (
+        !(await validateStructure(
+          filesTestArray[0],
+          await readFileAsText(value)
+        ))
+      ) {
+        console.log("123");
+        return;
       }
-    filesTestArray.push(await readFileAsText(value))
+    }
+    filesTestArray.push(await readFileAsText(value));
   }
 
   return new Promise((resolve, reject) => {
     resolve(filesTestArray);
-  })
-}
+  });
+};
 
 // Function for reading the uploaded file as a text
 const readFileAsText = async (file) => {
@@ -157,15 +166,18 @@ const readFileAsText = async (file) => {
 
     fr.readAsText(file);
   });
-}
+};
 
 // Function for downloading the result
 const download = (filename, text) => {
-  let element = document.createElement('a');
-  element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', filename);
-  element.style.display = 'none';
+  let element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:application/json;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
+  element.style.display = "none";
   document.body.appendChild(element);
   element.click();
   document.body.removeChild(element);
-}
+};
